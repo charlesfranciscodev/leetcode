d9b9b6ab9c4f7ae1d7ac729f299ae3150a4b868d
Charles-Antoine Francisco <charlesfranciscodev@users.noreply.github.com>
2019-06-10 09:11:11 -0400

    Create max_subarray.py

---
+++ easy/max_subarray.py
@@ -0,0 +1,10 @@
+import math
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        current_sum = max_sum = -math.inf
+        for num in nums:
+            current_sum = max(num, current_sum + num)
+            max_sum = max(max_sum, current_sum)
+        return max_sum
