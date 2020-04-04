e1353f083e3a99429f7d45e764f9d3cb51194c6d
Charles-Antoine Francisco <charlantfr@gmail.com>
2020-04-03 21:31:26 -0400

    Missing Number - optimal solution

--- python/missing_number.py
+++ python/missing_number.py
@@ -1,12 +1,4 @@
 class Solution:
-    def missingNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        x = 0
-        for i in range(1, len(nums) + 1):
-            x += i
-        for j in range(0, len(nums)):
-            x -= nums[j]
-        return x
+    def missingNumber(self, nums: List[int]) -> int:
+        total = len(nums) * (len(nums) + 1) // 2  # Gauss Formula
+        return total - sum(nums)
