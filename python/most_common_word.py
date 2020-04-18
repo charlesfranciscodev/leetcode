70dee9b76ae709332dd7b9a7ec90225529bc853a
Charles-Antoine Francisco <charlantfr@gmail.com>
2020-04-17 20:13:07 -0400

    Most Common Word - Review

--- python/most_common_word.py
+++ python/most_common_word.py
@@ -4,13 +4,10 @@ from collections import Counter
 
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        banned_set = set(banned)
-        regex = r"\w+"
-        words = re.findall(regex, paragraph)
-        lowercase_words = map(str.lower, words)
-        counter = Counter(lowercase_words)
-        most_common_word, count = counter.most_common(1)[0]
-        while most_common_word in banned_set:
-            del counter[most_common_word]
-            most_common_word, count = counter.most_common(1)[0]
-        return most_common_word
+        paragraph = paragraph.lower()
+        words = re.findall(r"\w+", paragraph)
+        counter = Counter()
+        for word in words:
+            if word not in banned:
+                counter[word] += 1
+        return counter.most_common(1)[0][0]
